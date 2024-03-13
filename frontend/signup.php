6f3db199328cfa091c3da78c9d381db7e671bd5d
dipaashu <aashudip120104@proton.me>
2024-03-13 09:15:30 +0530

    signup working

--- frontend/signup.php
+++ frontend/signup.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+    header("Location: index.php");
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -23,18 +30,87 @@
             </ul>
         </nav>
         <!-- sign up form  -->
+        <form action="./signup.php" method="post">
         <div class="login">
+            <?php
+            if (isset($_POST["submit"])) {
+                $fname = $_POST["fname"];
+                $uname = $_POST["uname"];
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+                $repass = $_POST["repass"];
+
+                $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+                $errors = [];
+
+                if (empty($fname) OR empty($uname) OR empty($email) OR empty($password) OR empty($repass)) {
+                    $errors[] = "All fields are required";
+                }
+
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $errors[] = "Invalid email address";
+                }
+                if (strlen($password) < 8) {
+                    $errors[] = "Password must be at least 8 characters";
+                }
+                if ($password!= $repass) {
+                    $errors[] = "Passwords do not match";
+                }
+
+                require_once './db.php';
+                $sql = "SELECT * FROM signup WHERE uname = '$uname'";
+                $result = mysqli_query($conn, $sql);
+                $rowCount = mysqli_num_rows($result);
+                if ($rowCount > 0) {
+                    $errors[] = "Username already exists";
+                }
+                $sql = "SELECT * FROM signup WHERE email = '$email'";
+                $result = mysqli_query($conn, $sql);
+                $rowCount = mysqli_num_rows($result);
+                if ($rowCount > 0) {
+                    $errors[] = "Email already exists";
+                }
+                
+                if(!empty($errors)) {
+                    echo "<div class='error' style='color:#eebbc3; font-family: 'Anta', sans-serif;'>";
+                    foreach ($errors as $error) {
+                        echo $error . '<br>';
+                    }
+                    echo "</div>";
+
+                }
+                
+                else {
+                    $sql = "INSERT INTO signup (`fname`, `uname`, `email`, `password`) VALUES (?, ?, ? ,?)";
+                    $stmt = mysqli_stmt_init($conn);
+                    $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+                    // echo $sql;
+                    if ($prepareStmt) {
+                        mysqli_stmt_bind_param($stmt, "ssss", $fname, $uname, $email, $passwordHash);
+                        // mysqli_stmt_execute($stmt);
+                        echo "<p style='color:#fffffe;'>Account created successfully. <a href='./login.php' style='text-decoration:None;color:#eebbc3'>Login</a></p>";
+                    }else{
+                        echo ("Error: ".mysqli_error($conn));
+                        die("Error: ".mysqli_error($conn));
+                    }
+                }
+            }
+            ?>
             <h1>Sign up</h1>
-            <div class="input"><input type="text" placeholder="Full name"></div>
-            <div class="input"><input type="text" placeholder="Username"></div>
-            <div class="input"><input type="email" placeholder="Email"></div>
-            <div class="input"><input type="password" placeholder="Password"></div>
-            <div class="input"><input type="password" placeholder="Confirm password"></div>
-            <div class="input"><input type="submit" value="Create account"></div>
+            <form action="signup.php">
+                <div class="input"><input type="text" name="fname" id="name"     placeholder="Full name"></div>
+                <div class="input"><input type="text" name="uname" id="uname"   placeholder="Username"></div>
+                <div class="input"><input type="email" name="email" id="email"  placeholder="Email"></div>
+                <div class="input"><input type="password" name="password" id="password"     placeholder="Password"></div>
+                <div class="input"><input type="password" name="repass" id="cpass"   placeholder="Confirm password"></div>
+                <div class="input"><input type="submit" name="submit" value="Create account"></div>
+            </form>
             <div class="links">
                 <a href="./index.php">Go to home page</a>
                 <a href="./login.php">Already have an account</a>
             </div>
         </div>
+        </form>
     </body>
 </html>
