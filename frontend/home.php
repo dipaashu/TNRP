5f9aa10f253068d69a3e5ccc422e86b0cb87c95a
dipaashu <aashudip120104@proton.me>
2024-03-26 18:28:48 +0530

    home page after login

--- frontend/home.php
+++ frontend/home.php
@@ -48,8 +48,24 @@ $notes = $noteOps->readNotes($userId);
 <html>
 <head>
     <title>My Notes</title>
+    <meta name="description" content="A repository system for notes">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="./img/favicon.png" />
+    <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/default.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anta&display=swap" rel="stylesheet">
 </head>
 <body>
+        <nav class="navbar">
+            <ul>
+                <li><a href="logout.php">Logout</a></li>
+                <li><a href="index.php">Home</a></li>
+                <a href="./index.php"><img id="logo" src="./img/logo.png" alt="Logo"></a>
+            </ul>
+        </nav>
     <h2>My Notes</h2>
     <form method="post">
         <input type="text" name="title" placeholder="Title" required>
